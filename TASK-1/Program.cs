56a2fd1f8c18fea08ca4d805ec51dda95c84c196
Vlad <vlad.melnik@bk.ru>
2022-07-15 23:53:54 +0300

    task 1 added

---
+++ TASK-1/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    for(int j = 0; j < size; j++) 
+    {
+       Console.Write($"Input {j + 1} element: "); 
+       newArray[j] = Convert.ToInt32(Console.ReadLine()); 
+    }
+    Console.WriteLine();  
+     return newArray; 
+}
+int Sumelements(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+
+        if(array[i]>0) sum++;
+    return sum;
+}
+void ShowArray(int[] array) 
+{ 
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " "); 
+ 
+    Console.WriteLine(); 
+} 
+ 
+Console.Write("Input size of array: "); 
+int size = Convert.ToInt32(Console.ReadLine()); 
+int[] array = CreateArray(size);
+ShowArray(array);
+int SumEL = Sumelements(array);
+Console.WriteLine("Positive sum is " + SumEL);
---
+++ TASK-2/Program.cs
@@ -0,0 +1,19 @@
+﻿double Orthocenter(double k1, double k2, double b1, double b2)
+{
+    double x = (b2 - b1)/(k1 - k2);
+    return x;
+}
+Console.WriteLine("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+if(k1==k2) 
+Console.WriteLine("Please note k1 and k2 cannot be equal");
+else
+Console.WriteLine("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+double x = Orthocenter(k1, k2, b1, b2);
+double y = k1*x+b1;
+Console.WriteLine("Crosspoint is " + (x + "," + y)); 
